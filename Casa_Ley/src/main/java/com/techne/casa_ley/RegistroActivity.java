ac0ab35332e1010d8a8034315deb221b3563f84d
Victor <crymore3@gmail.com>
2013-10-18 22:05:58 -0600

    Registrate

---
+++ Casa_Ley/src/main/java/com/techne/casa_ley/RegistroActivity.java
@@ -0,0 +1,23 @@
+package com.techne.casa_ley;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class RegistroActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.registro, menu);
+        return true;
+    }
+    
+}
